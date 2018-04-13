32190c074a524e811da284490e68490e4bba6df7
vdinovi <vito.dinovi@gmail.com>
2018-04-12 19:49:44 -0700

    successful implementation

--- src/apriori.py
+++ src/apriori.py
@@ -1,22 +1,28 @@
 import csv, pdb, itertools
+from pprint import pprint
+
 
 def parse_bakery(filename):
     baskets = {}
     with open(filename, 'r') as file:
         reader = csv.reader(file)
         for row in reader:
-            baskets[int(row[0])] = set(map(lambda x: int(x), row[1:-1]))
+            baskets[int(row[0])] = set(map(lambda x: int(x), row[1:]))
     return baskets
 
-def support(T, item_set):
-    x_subset_basket = 0
+def support(T, item_set, supports):
+    count = 0
+    iset = frozenset(item_set)
+    if iset in supports:
+        return supports[iset]
     for _, basket in T.items():
         if item_set.issubset(basket):
-            x_subset_basket += 1 
-    return (1.0 * x_subset_basket) / len(T)
+            count += 1 
+    supports[iset] = (1.0 * count) / len(T)
+    return supports[iset]
 
-def confidence(T, antecedent, consequent):
-    return support(T, antecedence.union(consequent)) / support(T, antecedent)
+def confidence(T, a, b, supports):
+    return support(T, a.union(b), supports) / support(T, a, supports)
 
 # From itertools recipe "pairwise"
 def pairwise(iterable):
@@ -29,66 +35,99 @@ def powerset(iterable):
     s = list(iterable)
     return itertools.chain.from_iterable(itertools.combinations(s, r) for r in range(len(s)+1))
 
-def candidates_gen(f, k):
+def candidates_gen(F, k):
     candidates = []
-    for f1, f2 in pairwise(f):
-        if len(f1) != k or len(f2) != k:
+    #for f1,f2 in (pair for pair in pairwise(F) if len(pair[0]) == len(pair[1]) == k):
+    for f1 in F:
+        if len(f1) != k:
             continue
-        c = frozenset(f1.union(f2))
-        if len(c) != len(f1) + 1:
-            continue
-        flag = True
-        for s in powerset(c):
-            if len(s) == len(c) - 1 and (set(s) not in f):
-                flag = False
-        if flag:
-            candidates.append(c)
+        for f2 in F:
+            if len(f1) != len(f2):
+                continue
+            c = f1.union(f2)
+            if len(c) == len(f1) + 1:
+                flag = True
+                for s in powerset(c):
+                    if len(s) == len(c) - 1 and (set(s) not in F):
+                        flag = False
+                if flag:
+                    candidates.append(c)
     return candidates
 
-def apriori(T, I, min_sup):
-    freq_sets = []
-    freq_sets.append([frozenset({i}) for i in I if support(T, {i}) >= min_sup])
+def apriori(T, I, min_sup, supports):
+    freq_sets = [[], [frozenset({i}) for i in I if support(T, {i}, supports) >= min_sup]]
     k = 2
-    while freq_sets[-1]:
-        candidates = candidates_gen(freq_sets[-1], k-1)
+    while freq_sets[k-1]:
+        candidates = candidates_gen(freq_sets[k-1], k-1)
         counts = {c: 0 for c in candidates}
         for _, basket in T.items():
-            cands = [c for c in candidates if c.issubset(basket)]
-            for c in cands:
+            for c in (c for c in candidates if c.issubset(basket)):
                 counts[c] += 1
         freq_sets.append([c for c in candidates if (counts[c]/len(T) >= min_sup)])
         k += 1
-    return [f for fs in freq_sets for f in fs]
+    return [set(f) for fs in freq_sets for f in fs]
 
+def get_skyline(F):
+    uniq = {frozenset(s) for s in F}
+    skyline = []
+    while uniq:
+        s = uniq.pop()
+        if all((not s.issubset(s1) for s1 in skyline)):
+            skyline.append(set(s))
+    return skyline
 
-def gen_rules(F, min_conf):
+def gen_rules(F, T, min_conf, supports):
     rules = []
-    singletons = [s for s in F if len(s) == 1]
-    for f in F - singletons:
+    for f in (f for f in F if len(f) >= 2):
         base = []
         for b in f:
             a = f - {b}
-            if confidence(T, a, b) >= min_conf:
-                base.append((a, b))
+            if confidence(T, a, {b}, supports) >= min_conf:
+                base.append((a, {b}))
         rules += base
     return rules
 
+"""
+lab-2-example-output
+
+Rule 1:     36   ---> 15    [sup=13.886113   conf=79.88506]
+Rule 2:     15   ---> 36    [sup=13.886113   conf=75.13513]
+Rule 3:     22   ---> 9    [sup=18.081919   conf=84.18605]
+Rule 4:     9   ---> 22    [sup=18.081919   conf=80.44444]
+Rule 5:     49   ---> 1    [sup=12.687312   conf=78.395065]
+Rule 6:     1   ---> 49    [sup=12.687312   conf=81.410255]
+Rule 7:     14, 16   ---> 12    [sup=25.674326   conf=99.612404]
+Rule 8:     12, 16   ---> 14    [sup=25.674326   conf=99.2278]
+Rule 9:     12, 14   ---> 16    [sup=25.674326   conf=95.89552]
+"""
+
+
+"""
+Rules2.xml
+
+Rule 1:    1       ---> 49   [15, 95]
+Rule 2:    15      ---> 36   [20, 95]
+Rule 3:    9       ---> 22   [25, 95]
+Rule 4:    12, 14  ---> 16   [30, 95]
+"""
+
+def rules_to_str(rules):
+    return ["{} ---> {}".format(r[0], r[1]) for r in rules]
+
+
 
 if __name__ == "__main__":
-    baskets = parse_bakery('../dataset/1000/1000-out1.csv')
-    item_set = [1,2,3]
-    min_sup = 0.01
-    #min_conf = 0.5
-    #cases = [
-    #        (.15, .95, ({1},     {49}),
-    #        (.20, .95, ({15},    {36}), 
-    #        (.25, .95, ({9},     {22}), 
-    #        (.30, .95, ({12,14}, {16})
-    #]
-    #for i in cases:
-    #    test(baskets, i)
-    freq_itemsets = apriori(baskets, baskets.keys(), min_sup)
-    pdb.set_trace()
-    #rules = gen_rules(freq_itemsets, min_conf)
+    baskets = parse_bakery('../dataset/example/out1.csv')
+    min_sup = 0.13
+    min_conf = 0.79
+    items = set(baskets.keys())
+    supports = {}
+    freq_isets = apriori(baskets, items, min_sup, supports)
+    skyline_freq_isets = get_skyline(freq_isets)
+    rules = gen_rules(skyline_freq_isets, baskets, min_conf, supports)
+    print("minSupp={}, minConf={}".format(min_sup, min_conf))
+    print("Skyline Sets: ", skyline_freq_isets)
+    for r in rules_to_str(rules):
+        print(r)
 
 
